b745d4f97a06dda5da4193a0f725a481c5518f57
zerkijulian92 <zerkijuliandri92@gmail.com>
2020-06-01 15:29:57 +0700

    RecyclerView Mode List Fix

--- app/src/main/java/com/androidpemula/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/androidpemula/myrecyclerview/MainActivity.kt
@@ -2,10 +2,65 @@ package com.androidpemula.myrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.androidpemula.myrecyclerview.adapter.ListHeroAdapter
 
 class MainActivity : AppCompatActivity() {
+    // Deklarasi Komponen View yang akan dimanipulasi
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // CastingView
+        rvHeroes    = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        // Memanggil data yang telah dibuat di HeroesData
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    // Membuat Function showRecylerList
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager  = LinearLayoutManager(this)
+        val listHeroAdapter     = ListHeroAdapter(list)
+        rvHeroes.adapter        = listHeroAdapter
+    }
+
+    // Menambahkan menu pada actionbar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inisialisai menu
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Inisialisai Item yang ada pada menu
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        // Casting item menu
+        when (selectedMode) {
+            R.id.action_list -> {
+                //kosong
+            }
+
+            R.id.action_grid -> {
+                //kosong
+            }
+
+            R.id.action_cardview -> {
+                //kosong
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/com/androidpemula/myrecyclerview/adapter/ListHeroAdapter.kt
@@ -0,0 +1,53 @@
+package com.androidpemula.myrecyclerview.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.androidpemula.myrecyclerview.Hero
+import com.androidpemula.myrecyclerview.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+/*NOTE:
+* Sebaiknya melakukan resolve dengan cara manual
+* */
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Inisialisasi View yang ada di dalam layout Item_row_hero.xml
+        var tvName: TextView    = itemView.findViewById(R.id.tv_item_name)
+        var tvDetail: TextView  = itemView.findViewById(R.id.tv_item_detail)
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ListViewHolder { // perhatikan ViewGroupnya
+        // Oncreate
+        val view: View = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_row_hero, viewGroup, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listHero.size
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        // OnBind
+        val  hero   = listHero[position]
+
+        // Memasukan gambar
+        Glide.with(holder.itemView.context)
+            .load(hero.photo)
+            .apply(RequestOptions().override(55, 55))
+            .into(holder.imgPhoto)
+
+        // Memasukan TextView
+        holder.tvName.text   = hero.name
+        holder.tvDetail.text = hero.detail
+    }
+
+}
